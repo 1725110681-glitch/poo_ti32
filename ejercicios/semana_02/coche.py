bf80be95af22ccd828a8d9e3af6b407018d8759f
MiguelCoro19 <1725110681@utectulancingo.edu.mx>
2026-05-18 13:39:53 +0000

    CREATED .gitignore

--- ejercicios/semana_02/coche.py
+++ ejercicios/semana_02/coche.py
@@ -24,3 +24,22 @@ class Coche:
         print(f"Ano:{self.ano}")
 
 porsche = Coche( "porsche", "911 carrera", "negro", "porsche_911", "12500", "3.0L biturbo", "2", "gasolina", "automatica PDK", "2025" )
+
+class Coche:
+    def encender(self):
+        print("Encendiendo el motor del Porsche")
+    def frenar (self):
+        print("El coche esta frenando con precision")
+    def cargar_combustible (self):
+        print("Llenando el tanque de gasolina")
+    def acelerar (self):
+        print("Acelerando a maxima potencia")
+    def encender_luces (self):
+        print("Encendiendo las luces del carro") 
+
+porsche = Coche()
+porsche.encender()
+porsche.frenar()
+porsche.cargar_combustible()
+porsche.acelerar()
+porsche.encender_luces() 
